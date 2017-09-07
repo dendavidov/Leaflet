29b6d2a0b713805515f779b8615c2eca06df863c
dendavidov <mail@dendavidov.com>
2017-09-07 13:47:24 +0300

    remove L from global

--- src/Leaflet.js
+++ src/Leaflet.js
@@ -25,14 +25,14 @@ export * from './map/index';
 
 // misc
 
-var oldL = window.L;
-export function noConflict() {
-	window.L = oldL;
-	return this;
-}
+// var oldL = window.L;
+// export function noConflict() {
+// 	window.L = oldL;
+// 	return this;
+// }
 
 // Always export us to window global (see #2364)
-window.L = exports;
+// window.L = exports;
 
 import {freeze} from './core/Util';
 Object.freeze = freeze;
